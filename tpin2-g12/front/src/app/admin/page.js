9ce3454311d8505340c3ae996e500d6c7d766ca2
bolachong <pchong@pioix.edu.ar>
2025-10-17 08:46:22 -0300

    css
    css

--- tpin2-g12/front/src/app/admin/page.js
+++ tpin2-g12/front/src/app/admin/page.js
@@ -1,6 +1,7 @@
 "use client"
 
 import styles from "@/app/admin/admin.module.css"
+import Desplegable from "@/components/Desplegable"
 import FormA from "@/components/FormA"
 
 export default function admin() {
@@ -8,11 +9,6 @@ export default function admin() {
         <>
             <div className={styles.Div}>
                 <FormA
-                    classNameH2={styles.H2}
-                    contentH2="Admin"
-                    classNameH4={styles.H4}
-                    contentPrimerH4="Modificar usuario"
-                    classNameI={styles.Input}
                 ></FormA>
             </div>   
         </>
--- tpin2-g12/front/src/components/FormA.js
+++ tpin2-g12/front/src/components/FormA.js
@@ -2,12 +2,14 @@
 
 import Input from "./Input";
 import Button from "./Button";
+import Desplegable from "./Desplegable";
 
 export default function Form(props){
     return(
         <>
             <h2 className={props.classNameH2}>{props.contentH2}</h2>
             <h4 className={props.classNameH4}>{props.contentPrimerH4}</h4>
+            <Desplegable></Desplegable>
             <Input
                 className={props.classNameI}
                 type={props.type1}
