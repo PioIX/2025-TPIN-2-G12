5b13c8b987a26e3cd771f9e306753b511c33b4ea
morvega12 <mvega@pioix.edu.ar>
2025-10-29 11:43:21 -0300

    css
    css

--- tpin2-g12/front/src/app/blackjack/page.js
+++ tpin2-g12/front/src/app/blackjack/page.js
@@ -1,1 +1,11 @@
 "use client"
+import Modal from "@/components/Modal"
+import ModalColor from "@/components/ModalColor"
+
+export default function Bj() {
+    return (
+        <>
+            <ModalColor></ModalColor>
+        </>
+    )
+}
--- tpin2-g12/front/src/components/Modal.js
+++ tpin2-g12/front/src/components/Modal.js
@@ -1,11 +1,17 @@
 "use client";
 
+import styles from "@/components/Modal.module.css"
+import Button from "./Button";
+
 export default function Modal(props){
     return(
         <>
-            <div className="modal">
-                <div className="overlay">
+            <div className={styles.modal}>
+                <div className={styles.overlay}>
                     <h1>GANO {props.usuario}</h1>
+                    <Button className={styles.Button}
+                        text="X"
+                    ></Button>
                 </div>
             </div>
         </>
